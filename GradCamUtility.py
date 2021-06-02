5e0a01cba69ec45a6482c17d9600e6970c403764
Saurav Mishra <saurav.mishra@live.com>
2021-06-02 18:52:32 +0530

    GradCAM Updates
    1. Updates to existing files for CAM technique.
    
    2. Initial commit for GRADCAM technique

--- GradCamUtility.py
+++ GradCamUtility.py
@@ -1,21 +1,21 @@
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-DESCRIPTION: CAM class activation visualization.
+DESCRIPTION: Grad-CAM class activation visualization.
              A Utility/Helper class that contains helper methods for visualizing the class level activations of 
-             a Convolotional Neural Network. CAM visualizations help us know which regions, patterns of 
+             a Convolotional Neural Network. Grad-CAM visualizations help us know which regions, patterns of 
              the image the neural network is looking at, and activating the region around those patterns. To 
              visualize the activation maps, we would need the output of the LAST CONVOLUTIONAL LAYERS
              and the final CLASSIFICATION LAYERS.
              
-REFERENCE:   https://arxiv.org/abs/1512.04150
+REFERENCE:   (François Chollet, 2020)
+             François Chollet, (2020) Grad-CAM class activation visualization. 
+             [online] Available at: https://keras.io/examples/vision/grad_cam/ [Accessed 20 Jun. 2020].
 
-WEBSITE:     https://arxiv.org/abs/1512.04150
+WEBSITE:     https://keras.io/examples/vision/grad_cam/
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 # Import Libraries
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Model
-
 import cv2
 import numpy as np
 
@@ -24,14 +24,14 @@ from IPython.display import Image
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-class CAMUtilities():
-
+class GradCamUtils():
+    
     '''
-    A Utility/Helper class that contains helper methods for visualizing the class level activations of 
-    a Convolotional Neural Network. CAM visualizations help us know which regions, patterns of 
-    the image the neural network is looking at, and activating the region around those patterns. To 
-    visualize the activation maps, we would need the output of the LAST CONVOLUTIONAL LAYERS
-    and the final CLASSIFICATION LAYERS.
+    A Utility/Helper class that contains helper methods for visualizing the gradient of the 
+    class level activations of a Convolotional Neural Network. CAM visualizations help us know which 
+    regions, patterns of the image the neural network is looking at, and activating the region around 
+    those patterns. To visualize the activation maps, we would need the output of the LAST CONVOLUTIONAL 
+    LAYERS.
     
     Args:
         model (keras model): the classification model.
@@ -41,15 +41,20 @@ class CAMUtilities():
     
     '''
     
-    def __init__(self, model, layerName):
+    def __init__(self, model, last_conv_layer_name):
         
         self.model = model
-        self.layerName = layerName
+        self.last_conv_layer_name = last_conv_layer_name
+        
+        # get the layer object of the last convolutionl layer
+        last_conv_layer = self.model.get_layer(self.last_conv_layer_name).output
+        
+        # create a model that maps the input image to the activations
+        # of the last conv layer as well as the output predictions
+        self.grad_model = tf.keras.models.Model(
+            [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output])
 
-        last_conv_layer = self.model.get_layer(self.layerName).output
-        self.cammodel = Model(inputs=self.model.input,
-                                       outputs=[last_conv_layer, self.model.output])
-    
+
     def GetImageArrayInBatch(self, img_path, size):
         
         '''
@@ -63,9 +68,8 @@ class CAMUtilities():
         Return Value: 
             img_array - The array representation (in terms of the batch - 
                         (1, size_x, size_y, channel)) of the image.
-        
         '''
-
+        
         img = keras.preprocessing.image.load_img(img_path, target_size=size)
 
         # Get the image array
@@ -74,10 +78,10 @@ class CAMUtilities():
         # We add a dimension to transform our array into a "batch"
         # of size (1, size_x, size_y, channel)
         img_array = np.expand_dims(array, axis=0)
-        
         return img_array
 
-    def compute_heatmap(self, image):
+
+    def compute_heatmap(self, img_array, pred_index=None):
         
         '''
         Purpose: 
@@ -91,7 +95,8 @@ class CAMUtilities():
                            visualized.
             2. model - the CNN model whose activations are to be analyzed.
             3. last_conv_layer_name - name of the last convolutional layer of the model.
-            4. classifier_layer_names - the final classification layers.
+            4. pred_index (optional): The index of the largest value in the tensor. 
+                                      The unit which contributed the most towards the predicted result.
 
         Return Value: 
             heatmap - The heatmap showing the more active regions the CNN looked at in deciding the class
@@ -99,41 +104,35 @@ class CAMUtilities():
                       
         '''
         
-        # conv_outputs is the output activations of the layer 
-        # pointed to by the self.layerName
-        # predictions is the predicted result of the image.
-        [conv_outputs, predictions] = self.cammodel.predict(image)
-        # print(conv_outputs.shape) --> (1, 33, 33, 256)
-        
-        conv_outputs = conv_outputs[0, :, :, :]
-        # print(conv_outputs.shape) --> (33, 33, 256)
-        
-        # make the conv_outputs dimension to have the channels first.
-        # Roll the specified axis backwards, 
-        # until it lies in a given position.
-        conv_outputs = np.rollaxis(conv_outputs, 2)
-        # print(conv_outputs.shape) --> (256, 33, 33)
-        
-        # get the weights pertaining to the predicted class.
-        class_weights = self.model.layers[-1].get_weights()[0]
-        # print(self.model.layers[-1]) --> Dense layer
-        
-        # Create the class activation map.
-        cam = np.zeros(shape = conv_outputs.shape[1:3], dtype=np.float32)
+        # Compute the gradient of the top predicted class for our input image
+        # with respect to the activations of the last conv layer
+        with tf.GradientTape() as tape:
+            last_conv_layer_output, preds = self.grad_model(img_array)
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0]) # returns the index of the largest value in the tensor.
+            class_channel = preds[:, pred_index]
 
-        for idx, weight in enumerate(class_weights[:]):
-            cam += weight * conv_outputs[idx, :, :]
+        # This is the gradient of the output neuron (top predicted or chosen)
+        # with regard to the output feature map of the last conv layer
+        grads = tape.gradient(class_channel, last_conv_layer_output)
 
-        cam /= np.max(cam)
-        cam = cv2.resize(cam, (image.shape[1], image.shape[2]))
+        # This is a vector where each entry is the mean intensity of the gradient
+        # over a specific feature map channel.
+        # get the mean of elements across dimensions of a tensor.
+        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-        heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
-        
-        # get the region of interest of the model, which part of the
-        # image the model is looking at...
-        heatmap[np.where(cam < 0.3)] = 0
+        # We multiply each channel in the feature map array
+        # by "how important this channel is" with regard to the top predicted class
+        # then sum all the channels to obtain the heatmap class activation
+        last_conv_layer_output = last_conv_layer_output[0]
+        heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+        heatmap = tf.squeeze(heatmap)
 
-        return heatmap
+        # For visualization purpose, we will also normalize the heatmap between 0 & 1
+        heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+
+        return heatmap.numpy()
+    
     
 
     def DisplayHeatMap(self, heatmap):
@@ -147,48 +146,52 @@ class CAMUtilities():
 
         Return Value: 
             NONE
-            
+
         '''
-            
+        
         # Display heatmap
         plt.matshow(heatmap)
         plt.axis('off')
         plt.show()
 
 
-
-    def GetSuperImposedCAMImage(self, heatmap, image):
+    
+    def GetSuperImposedCAMImage(self, heatmap, image, alpha = 0.5):
         
-        '''
+        ''''
         Purpose: 
             Get the super imposed or the blended version of the image comprising of the class activations
             heatmap and the original image.
 
         Parameters:
             1. heatmap - The class activation heatmap for the concerned image.
-            2. img - The original image for which the activation image is being calculated.
+            2. image - The original image for which the activation image is being calculated.
+            3. alpha (optional) - A number between 0 & 1 to control the weighted blending of the images.
 
         Return Value: 
             superImposedImage - The blended version of the original image and the corresponding class 
                                 activation map.
-                                
+
         '''
-            
-        # Rescale heatmap to a range 0-255
+        
+        # Rescale the image to 0-255
+        image = image * 255
+
+        # Rescale heatmap to 0-255
+        # Resize to the size of the image
+        # Apply colormap
         heatmap = np.uint8(255 * heatmap)
-        
-        # convert the image from BGR format to the RGB format.
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
-        # Rescale the image
-        image = np.uint8(255 * image)
-       
-        superImposedImage = cv2.addWeighted(heatmap, 0.5, image, 0.5, 0.0)
+        heatmap = cv2.resize(heatmap, (image.shape[1], image.shape[0]))
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+        # Superimpose the heatmap on original image
+        superImposedImage = heatmap * alpha + image
+        superImposedImage = tf.keras.preprocessing.image.array_to_img(superImposedImage)
         
         return superImposedImage
 
     
-    def DisplaySuperImposedImages(self, image, heatmap, superimposed_img):
+    def DisplaySuperImposedImages(self, image, heatmap,superimposed_img):
         
         '''
         Purpose: 
@@ -204,18 +207,13 @@ class CAMUtilities():
             
         '''
         
-        # Convert the image from BGR format to the RGB format.
-        # Rescale the image
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = np.uint8(255 * image)
-        
         fig, ax = plt.subplots(1, 3, figsize=(8, 12))
 
         ax[0].imshow(image)
         ax[1].imshow(heatmap)
         ax[2].imshow(superimposed_img)
 
-        ax[0].title.set_text('Original Image')
+        ax[0].title.set_text('Original\nImage')
         ax[1].title.set_text('Class\nActivation\nHeatmap')
         ax[2].title.set_text('Class\nActivation\nBlended\nImage')
 
